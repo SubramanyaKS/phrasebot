152ac1950a16dd4d5d875252bf3b183416915224
SubramanyaKS <subramanyaks2205@gmail.com>
2025-02-08 17:39:35 +0530

    added the reusable components for phrasebot

---
+++ lib/components/home_drawer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:phrasebot/screens/welcome_screen.dart';
+import 'package:phrasebot/services/auth_service.dart';
+import 'package:phrasebot/utils/constant.dart';
+import 'package:phrasebot/screens/profile.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Color(0xFF0000FF),
+        child: ListView(
+          children: [
+            DrawerHeader(
+                child:
+                Center(child: Text(title.toUpperCase(), style:TextStyle(fontSize: 35,color: Colors.white)))
+            ),
+            ListTile(
+              leading: Icon(Icons.person,color: Colors.white,),
+              onTap: (){Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => Profile(),
+                ),
+              );},
+              title: Text("Profile",style: TextStyle(fontSize: 24,color: Colors.white),),
+
+            ),
+            ListTile(
+              leading: Icon(Icons.logout,color: Colors.white,),
+              onTap: (){signOut();
+              Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => WelcomeScreen(),
+                      ),
+                    );
+                },
+              title: Text("SignOut",style: TextStyle(fontSize: 24,color: Colors.white),),
+
+            ),
+
+          ],
+        ),
+
+      ),
+    );
+  }
+}
